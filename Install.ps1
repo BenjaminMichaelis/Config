8693b34dd64d3ddcf14458c4b89d7de8ebab73b1
Mark Michaelis <mark@intelliTect.com>
2020-03-24 07:30:49 -0700

    Update and rename install.ps1 to Install.ps1

--- Install.ps1
+++ Install.ps1
@@ -23,7 +23,7 @@ function Add-ScoopBucket {
 
   # Git is required when adding an additional scoop bucket.
   if(-not (Get-Command git -ErrorAction Ignore)) {
-    choco install git -y /GitOnlyOnPath /NoAutoCrlf
+    choco install git -y --params "/GitOnlyOnPath /NoAutoCrlf /NoShellHereIntegration"
     Set-Alias -Name Git -Value "$env:ProgramFiles\Git\cmd\git.exe"
   } 
 
