0fcc10790efb7661137021e3464774ab46dc4fbf
Mark Michaelis <mark@IntelliTect.com>
2020-03-25 11:13:02 -0700

    Set scoop install directory to $env:ProgramData\scoop

--- install.ps1
+++ install.ps1
@@ -1,16 +1,18 @@
-if((Get-ExecutionPolicy) -eq 'Restricted') {
+if ((Get-ExecutionPolicy) -eq 'Restricted') {
   Set-ExecutionPolicy Bypass -Scope Process -Force
 }
 
 #Install Chocolatey
-if(-not (Get-Command choco -ErrorAction Ignore)) {
+if (-not (Get-Command choco -ErrorAction Ignore)) {
   [System.Net.ServicePointManager]::SecurityProtocol = [System.Net.ServicePointManager]::SecurityProtocol -bor 3072; 
   Invoke-Expression ((New-Object System.Net.WebClient).DownloadString('https://chocolatey.org/install.ps1'))
 }
 
 #Install Scoop
-if(-not (Get-Command scoop -ErrorAction Ignore)) {
-  Invoke-WebRequest -useb get.scoop.sh | Invoke-Expression
+if (-not (Get-Command scoop -ErrorAction Ignore)) {
+  $env:SCOOP = "$env:ProgramData\scoop"
+  [environment]::SetEnvironmentVariable('SCOOP', $env:SCOOP, 'Machine')
+  Invoke-WebRequest -UseBasicParsing get.scoop.sh | Invoke-Expression
 }
 
 
@@ -22,13 +24,13 @@ function Add-ScoopBucket {
   )
 
   # Git is required when adding an additional scoop bucket.
-  if(-not (Get-Command git -ErrorAction Ignore)) {
+  if (-not (Get-Command git -ErrorAction Ignore)) {
     choco install git -y --params "/GitOnlyOnPath /NoAutoCrlf /NoShellHereIntegration"
     # Set-Alias -Name git -Value "$env:ProgramFiles\Git\cmd\git.exe"
-    $env:Path="$env:Path;$env:ProgramFiles\Git\cmd\"
+    $env:Path = "$env:Path;$env:ProgramFiles\Git\cmd\"
   } 
 
-  if((scoop bucket list) -notcontains $name) {
+  if ((scoop bucket list) -notcontains $name) {
     Write-Host "scoop bucket add $name $url"
     # Run in new PowerShell process to ensure git path is enabled.
     # powershell -ExecutionPolicy unrestricted -Command scoop bucket add $name $url
