70a249a925d5b02ea6a5df3db8bdacaa46e24c91
Mark Michaelis <mark@IntelliTect.com>
2020-03-25 10:15:21 -0700

    Added SetPowerConfiguration

---
+++ bucket/SetPowerConfiguration.ps1
@@ -0,0 +1,10 @@
+# Battery
+powercfg /change monitor-timeout-dc 5
+powercfg /change standby-timeout-dc 10
+powercfg /change hibernate-timeout-dc 60
+
+# Plugged In
+powercfg /change monitor-timeout-ac 10
+powercfg /change standby-timeout-ac 0
+powercfg /change hibernate-timeout-ac 0
+
