73a92a0be78a6ca58060a51f48f5ecf969184c90
Mark Michaelis <mark@IntelliTect.com>
2020-03-29 05:58:19 -0700

    Refactored common functions into Utils.ps1 file.

--- bucket/OSBase.ps1
+++ bucket/OSBase.ps1
@@ -1,29 +1,11 @@
 
-Function Test-Command {
-    [CmdletBinding()]
-    param(
-        [string]$command
-    )
-    return [bool](get-command $command -ErrorAction Ignore)
-}
+. $PSScriptRoot\Utils.ps1
 
-# TODO: Consider writing as a filter.
-Function Test-ChocolateyPackageInstalled {
-    [CmdletBinding()]
-    [OutputType([bool])]
-    param(
-        [Parameter(Mandatory)][string]$PackageName
-    )
-
-    [bool] $installed = choco list $PackageName --local-only --no-progress | Where-Object {
-        $_ -match "$PackageName\s.*"
-    }
-    Write-Output $installed
-}
-
-'7zip', 'notepad2', 'Everything', 'GoogleChrome', 'SysInternals', 'WinDirStat' | Where-Object {
+'7zip', 'notepad2', 'Everything', 'GoogleChrome', 'SysInternals', 'WinDirStat', `
+    'microsoft-windows-terminal', 'fzf' | Where-Object {
     Test-ChocolateyPackageInstalled $_
 } | ForEach-Object { 
     choco install -y $_
 }
+
 
---
+++ bucket/Utils.ps1
@@ -0,0 +1,117 @@
+Function Test-Command {
+    [CmdletBinding()]
+    param(
+        [string]$command
+    )
+    return [bool](get-command $command -ErrorAction Ignore)
+}
+
+# TODO: Consider writing as a filter.
+Function Test-ChocolateyPackageInstalled {
+    [CmdletBinding()]
+    [OutputType([bool])]
+    param(
+        [Parameter(Mandatory)][string]$PackageName
+    )
+
+    [bool] $installed = choco list $PackageName --local-only --no-progress | Where-Object {
+        $_ -match "$PackageName\s.*"
+    }
+    Write-Output $installed
+}
+
+
+Function Import-ChocolateyModule {
+    if (test-path env:ChocolateyInstall) {
+        Import-Module (Resolve-Path -Path "$env:ChocolateyInstall\*\chocolateyInstaller.psm1").Path
+        if (Test-Path Function:\Write-Host) {
+            Remove-Item Function:Write-Host # Chocolatey overwrites Write-Host.  This call removes the override.  It should still occur within Chocolatey.
+            # Note that this is necessary otherwise Write-Host attempts to write to the chocolatey log file in Program Data and doesn't have
+            # permission outside of an admin prompt.
+        }
+        $env:ChocolateyAllowEmptyChecksumsSecure = $true
+        $env:ChocolateyAllowEmptyChecksums = $true
+        $env:ChocolateyPackageFolder = "$env:ChocolateyInstall\Lib"
+        Set-PackageSource -Name chocolatey -ProviderName Chocolatey -Trusted -Force
+    }
+    else {
+        throw "Chocolatey is not installed"
+    }
+}
+
+
+if (!(Test-Path function:Install-WebDownload)) {
+    Function Install-WebDownloadOfZip {
+        [CmdletBinding()]
+        param(
+            [Parameter(Mandatory)][string] $PackageName,
+            [Parameter(Mandatory)][alias("Uri")][string] $url,
+            $UnzipLocation = "$env:ChocolateyInstall\lib\$PackageName"
+        )
+
+        Import-ChocolateyModule
+
+        # See Chocolatey's Get-CheckSumValid.ps1 for more info.
+        $originalChocolateyAllowEmptyChecksums = $env:ChocolateyAllowEmptyChecksums
+        $originalChocolateyAllowEmptyChecksumsSecure = $env:ChocolateyAllowEmptyChecksumsSecure
+        try {
+            # Needed because Chocolatey is not setting up context.
+            if (!(test-path variable:\helpersPath)) {
+                $setHelpersPath = $true
+                $global:helpersPath = $env:ChocolateyInstall
+            }
+
+            $env:ChocolateyAllowEmptyChecksums = 'true'
+            $env:ChocolateyAllowEmptyChecksumsSecure = 'true'
+            Install-ChocolateyZipPackage -packageName $PackageName -url $url -unzipLocation $UnzipLocation -specificFolder ''
+            Get-ChildItem $UnzipLocation *.exe | ForEach-Object { Install-BinFile -name TrayIt -path $_.FullName }
+        }
+        finally {
+            if ($setHelpersPath) {
+                remove-item variable:\global:helpersPath
+            }
+            $env:ChocolateyAllowEmptyChecksums = $originalChocolateyAllowEmptyChecksums
+            $env:ChocolateyAllowEmptyChecksumsSecure = $originalChocolateyAllowEmptyChecksumsSecure
+        }
+    }
+
+
+    Function Install-WebDownload {
+        [CmdletBinding()] param(
+            [Parameter(Mandatory)][alias("Uri")][string] $url,
+            [Parameter(Mandatory)][string] $PackageName,
+            [Parameter(ParameterSetName = "CommandLine")] [string] $arguments = $null,
+            [Parameter(ParameterSetName = "ScriptBlock")][ScriptBlock] $postDownloadScriptBlock,
+            [Parameter(ParameterSetName = "UnattendedSilentSwitchFinder",
+                HelpMessage = "Lookup the unattended silent switch for the setup program.")][switch]$ussf,
+            [string] $installFileName = [System.Management.Automation.WildcardPattern]::Escape((Split-Path $url -Leaf)),
+            [switch]$forceDownload )
+
+        #TODO Switch to Get-ChocolateyWebFile and use Invoke-WebRequest as fallback.
+        $tempPath = Get-TempPath
+
+        if ([IO.Path]::GetExtension($InstallFileName) -eq ".zip") {
+            Install-WebDownloadOfZip -Uri $url -packageName $PackageName
+        }
+        else {
+            $installFileName = Join-Path $tempPath $installFileName
+
+            if ($forceDownload -OR ($installFileName -eq "Setup.exe") -OR !(Test-Path $installFileName) ) {
+                Invoke-WebRequest $url -OutFile $installFileName
+            }
+
+            if ($ussf) {
+                ussf $installFileName
+            }
+            else {
+                If ( ([string]::IsNullOrWhiteSpace($PsCmdlet.ParameterSetName)) -or ($PsCmdlet.ParameterSetName -eq "CommandLine") ) {
+                    $postDownloadScriptBlock = [ScriptBlock] {
+                        $process = Start-Process $installFileName $arguments -PassThru -wait
+                        return $process.ExitCode
+                    }
+                }
+            }
+            Write-Output (Invoke-Command $postDownloadScriptBlock)
+        }
+    }
+}
