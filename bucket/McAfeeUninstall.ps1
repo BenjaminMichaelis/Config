743e24bf13a1176ee7be642c6502c132a405d2d5
Mark Michaelis <mark@IntelliTect.com>
2020-03-30 07:38:25 -0700

    More

--- bucket/McAfeeUninstall.ps1
+++ bucket/McAfeeUninstall.ps1
@@ -1,7 +1,10 @@
 
 . $PSScriptRoot\Utils.ps1
 
+Write-Host "Uninstalling McAfee Applications..."
+
 Function Uninstall-McAfeeApplications {
+    Write-Host "Running $($MyInvocation.MyCommand.Name)..."
     
     Get-Program 'McAfee*' | ForEach-Object {
         try {
