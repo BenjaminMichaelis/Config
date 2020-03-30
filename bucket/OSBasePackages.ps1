f7a0c8c231e537f4658d1c8286bef09a11fcc802
Mark Michaelis <mark@IntelliTect.com>
2020-03-29 17:11:04 -0700

    Added diagnostics messages to OSBasePackages

--- bucket/OSBasePackages.ps1
+++ bucket/OSBasePackages.ps1
@@ -1,10 +1,12 @@
 
+Write-Host 'Installing and configuring OSBasePackages...'
 . $PSScriptRoot\Utils.ps1
 
 '7zip', 'notepad2', 'Everything', 'GoogleChrome', 'SysInternals', 'WinDirStat', `
     'microsoft-windows-terminal', 'fzf' | Where-Object {
     Test-ChocolateyPackageInstalled $_
 } | ForEach-Object { 
+    Write-Host "Installing $_..."
     choco install -y $_
 }
 
