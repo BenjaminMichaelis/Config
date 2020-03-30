0c03a76ab300fb4df2613543ae7e4756dccce4a4
Mark Michaelis <mark@IntelliTect.com>
2020-03-29 21:01:49 -0700

    Code cleanup

--- bucket/Chocolatey.ps1
+++ bucket/Chocolatey.ps1
@@ -3,10 +3,6 @@ Write-Host 'Installing and configuring Chocolatey...'
 . $PSScriptRoot\Utils.ps1
 
 Function Install-Chocolatey {
-    if (Get-Command -name choco -ErrorAction Ignore) {
-        Write-Information 'Chocolatey is already installed and configured.'
-        return
-    }
     
     if (-not (Test-Command choco)) {
         Write-Output "Installing Chocolatey..."
@@ -22,6 +18,7 @@ Function Install-Chocolatey {
     [Environment]::SetEnvironmentVariable("ChocolateyAllowEmptyChecksumsSecure", $true)
     [Environment]::SetEnvironmentVariable("ChocolateyToolsLocation", "$env:ChocolateyInstall\Tools")
 
+    # TODO: Figure repository for API Key
     if (Test-Path C:\data\Profile\ChocolateyAPIKey.txt) {
         Get-Content C:\data\Profile\ChocolateyAPIKey.txt | Foreach-Object { choco setapikey $_ }
     }
@@ -30,10 +27,13 @@ Function Install-Chocolatey {
 
     choco install au # Automatic Chocolatey Package Update
 
-    [string]$chocolateyLicenseFolder = (Join-Path "$env:ChocolateyInstall" 'License')
-    mkdir $chocolateyLicenseFolder
-    #TODO: Switch to use New-Item.
-    cmd /c mklink (Join-Path $chocolateyLicenseFolder chocolatey.license.xml) C:\Dropbox\Profile\chocolatey.license.xml
+    if (Test-Path C:\Dropbox\Profile\chocolatey.license.xml) {
+        [string]$chocolateyLicenseFolder = (Join-Path "$env:ChocolateyInstall" 'License')
+        mkdir $chocolateyLicenseFolder
+        # TODO: Figure out repository for chocolatey license.
+        #TODO: Switch to use New-Item.
+        cmd /c mklink (Join-Path $chocolateyLicenseFolder chocolatey.license.xml) C:\Dropbox\Profile\chocolatey.license.xml
+    }
 
     Import-ChocolateyModule
 }
