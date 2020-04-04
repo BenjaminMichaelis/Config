421c89e86499446c3af1a0a98209d6ca3ce1d448
Mark Michaelis <mark@IntelliTect.com>
2020-04-03 21:57:50 -0700

    Refactored out test for choco package install

--- bucket/OSBasePackages.ps1
+++ bucket/OSBasePackages.ps1
@@ -3,10 +3,7 @@ Write-Host 'Installing and configuring OSBasePackages...'
 . "$PSScriptRoot\Utils.ps1"
 
 '7zip', 'notepad2', 'Everything', 'GoogleChrome', 'SysInternals', 'WinDirStat', `
-    'microsoft-windows-terminal', 'fzf', 'procexp' | Where-Object {
-    Write-Host "Testing for install of chocolatey pacakge $_ $(Test-ChocolateyPackageInstalled $_)..."
-    -not (Test-ChocolateyPackageInstalled $_)
-} | ForEach-Object { 
+    'microsoft-windows-terminal', 'fzf', 'procexp' | ForEach-Object { 
     Write-Host "Installing $_..."
     choco install -y $_
 }
