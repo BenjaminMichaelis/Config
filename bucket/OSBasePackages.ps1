1e72a600698715a2a4a32051d987b97b888ac29f
Mark Michaelis <mark@IntelliTect.com>
2020-04-03 20:54:30 -0700

    Added Process Explorer

--- bucket/OSBasePackages.ps1
+++ bucket/OSBasePackages.ps1
@@ -3,7 +3,7 @@ Write-Host 'Installing and configuring OSBasePackages...'
 . "$PSScriptRoot\Utils.ps1"
 
 '7zip', 'notepad2', 'Everything', 'GoogleChrome', 'SysInternals', 'WinDirStat', `
-    'microsoft-windows-terminal', 'fzf' | Where-Object {
+    'microsoft-windows-terminal', 'fzf', 'procexp' | Where-Object {
     Write-Host "Testing for install of chocolatey pacakge $_ $(Test-ChocolateyPackageInstalled $_)..."
     -not (Test-ChocolateyPackageInstalled $_)
 } | ForEach-Object { 
