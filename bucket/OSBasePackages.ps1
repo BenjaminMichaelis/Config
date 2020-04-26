7758d18df8cf89adef7cc319ebce51ca606c7ebd
Mark Michaelis <mark@IntelliTect.com>
2020-04-26 11:49:37 -0700

    Added USSF install

--- bucket/OSBasePackages.ps1
+++ bucket/OSBasePackages.ps1
@@ -3,7 +3,7 @@ Write-Host 'Installing and configuring OSBasePackages...'
 . "$PSScriptRoot\Utils.ps1"
 
 '7zip', 'notepad2', 'Everything', 'GoogleChrome', 'SysInternals', 'WinDirStat', `
-    'microsoft-windows-terminal', 'fzf', 'procexp', 'powershell-core' | ForEach-Object { 
+    'microsoft-windows-terminal', 'fzf', 'procexp', 'powershell-core', 'ussf' | ForEach-Object { 
     Write-Host "Installing $_..."
     choco install -y $_
 }
