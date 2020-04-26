b00e5243b6243040c81298f036d290aab12748ac
Mark Michaelis <mark@IntelliTect.com>
2020-04-26 10:16:53 -0700

    Minor changes with host messages

--- bucket/McAfeeUninstall.ps1
+++ bucket/McAfeeUninstall.ps1
@@ -4,7 +4,6 @@ Write-Host "Uninstalling McAfee Applications..."
 . "$PSScriptRoot\Utils.ps1"
 
 Function Uninstall-McAfeeApplications {
-    Write-Host "Running $($MyInvocation.MyCommand.Name)..."
     Get-Program 'McAfee*' | ForEach-Object {
         try {
             Write-Host "Uninstalling $($_.Name)..."
