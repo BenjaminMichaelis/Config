a001f23da406c23eae5d2d607cde13708ec0b5b2
Benjamin Michaelis <benjamin@michaelis.net>
2022-04-15 15:23:08 -0700

    Update ClientBasePackages.ps1

--- bucket/ClientBasePackages.ps1
+++ bucket/ClientBasePackages.ps1
@@ -2,7 +2,7 @@
 Write-Host 'Installing and configuring OSBasePackages...'
 . "$PSScriptRoot\Utils.ps1"
 
-'' | ForEach-Object { 
+'bitwarden' | ForEach-Object { 
     Write-Host "Installing $_..."
     choco install -y $_
 }
