d21862d2f73a3333e0a253e8685fa7a3a0d451e0
Benjamin Michaelis <benjamin@michaelis.net>
2022-04-29 12:28:20 -0700

    Add screentogif

--- bucket/ClientBasePackages.ps1
+++ bucket/ClientBasePackages.ps1
@@ -2,7 +2,7 @@
 Write-Host 'Installing and configuring OSBasePackages...'
 . "$PSScriptRoot\Utils.ps1"
 
-'bitwarden' 'powertoys' 'spotify' | ForEach-Object { 
+'bitwarden' 'powertoys' 'spotify' 'screentogif' | ForEach-Object { 
     Write-Host "Installing $_..."
     choco install -y $_
 }
