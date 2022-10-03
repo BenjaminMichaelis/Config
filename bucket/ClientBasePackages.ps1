58c047ce7e83eff346bf9dc97f277c591b4522f3
Benjamin Michaelis <benjamin@michaelis.net>
2022-10-02 17:58:36 -0700

    Add a whole bunch of packages

--- bucket/ClientBasePackages.ps1
+++ bucket/ClientBasePackages.ps1
@@ -2,7 +2,7 @@
 Write-Host 'Installing and configuring OSBasePackages...'
 . "$PSScriptRoot\Utils.ps1"
 
-'bitwarden', 'powertoys', 'spotify', 'screentogif', 'brave', 'everything' | ForEach-Object { 
+'bitwarden', 'powertoys', 'spotify', 'screentogif', 'brave', 'everything', 'obs-studio', 'notepad2', 'paint.net', 'microsoft-windows-terminal', 'windirstat', 'todoist-desktop', 'googlechrome'  | ForEach-Object { 
     Write-Host "Installing $_..."
     choco install -y $_
 }
