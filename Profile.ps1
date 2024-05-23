e4526f4290616a14c66723c06b4bd53b3a16e711
Benjamin Michaelis <git@relay.benjamin.michaelis.net>
2024-05-23 14:58:36 -0700

    WIP

---
+++ Profile.ps1
@@ -0,0 +1,8 @@
+#f45873b3-b655-43a6-b217-97c00aa0db58 PowerToys CommandNotFound module
+
+Import-Module -Name Microsoft.WinGet.CommandNotFound
+#f45873b3-b655-43a6-b217-97c00aa0db58pws
+
+Import-Module z
+
+oh-my-posh init pwsh --config 'https://raw.githubusercontent.com/JanDeDobbeleer/oh-my-posh/main/themes/jandedobbeleer.omp.json' | Invoke-Expression
